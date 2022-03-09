b0566284a89c1856755b3f2d6b08bd45652506f7
Soul <xorekvblendere@mail.ru>
2022-03-09 18:57:31 +1000

    svin

--- CrackaSmile/ViewModels/EditClientViewModel.cs
+++ CrackaSmile/ViewModels/EditClientViewModel.cs
@@ -10,9 +10,69 @@ namespace CrackaSmile.ViewModels
 {
     public class EditClientViewModel: BaseViewModel
     {
+        public ClientApi AddClient { get; set; }
         public EditClientViewModel(ClientApi client)
         {
 
+            TakeListClients();
+
+            if (client == null)
+            {
+                //CreateNewClient();
+            }
+            else
+            {
+                AddClient = new ClientApi
+                {
+                    Name = client.Name,
+                    FatherName = client.FatherName,
+                    LastName = client.LastName,
+                    Address = client.Address,
+                    Email = client.Email,
+                    Telephone = client.Telephone
+                };
+
+
+            }
+        }
+
+
+        public async Task CreateNewClient()
+        {
+
+            ClientApi editClient = new ClientApi
+            {
+                Name = "test",
+                FatherName = "test",
+                LastName = "test",
+                Address = "test",
+                Email = "test",
+                Telephone = "test"
+
+            };
+            await Api.PostAsync<ClientApi>(editClient,"Client");
+        }
+
+        public async Task EditClient()
+        {
+
+            ClientApi editClient = new ClientApi
+            {
+                Name = "test",
+                FatherName = "test",
+                LastName = "test",
+                Address = "test",
+                Email = "test",
+                Telephone = "test"
+
+            };
+            await Api.PutAsync<ClientApi>(editClient, "Client");
+        }
+
+        public async Task TakeListClients()//вызов клиентов
+        {
+            var result = await Api.GetListAsync<ClientApi[]>("Client");
+            SignalChanged("clients");
         }
     }
 }
